fce42c0c91f5aad7369982517bdb90795dfd5170
huiergo <1063229610@qq.com>
2022-09-04 16:04:08 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -6,8 +6,8 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  base: '/heima/',
-  publicPath: '/heima/',
+  base: '/lib-1/',
+  publicPath: '/lib-1/',
   exportStatic: {},
   mode: 'site',
   // more config: https://d.umijs.org/config
